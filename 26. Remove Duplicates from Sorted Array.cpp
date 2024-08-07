1fcba812be4ed5c43a78452781cc44753023d40c
andleeb09 <100277360+andleeb09@users.noreply.github.com>
2024-08-07 13:47:32 +0530

    Remove Duplicates from Sorted Array.cpp

---
+++ 26. Remove Duplicates from Sorted Array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        auto i = unique(nums.begin(), nums.end());
+        nums.erase(i, nums.end());
+        return nums.size();   
+    }   
+};
