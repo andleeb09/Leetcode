45bd887147f31a4192079556dfa5f99f049d01e8
andleeb09 <100277360+andleeb09@users.noreply.github.com>
2024-08-13 11:35:04 +0530

    Update Insert into a Binary Search Tree.cpp

--- Insert into a Binary Search Tree.cpp
+++ Insert into a Binary Search Tree.cpp
@@ -9,6 +9,7 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+//WITH RECURSION
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
@@ -19,5 +20,37 @@ public:
        else 
        root->right=insertIntoBST(root->right,val);
        return root;    
+    }
+};
+
+//WITHOUT:USING 2 POINTER
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        if(!root){
+            TreeNode* node = new TreeNode(val);
+            return node;
+        }
+        TreeNode* node = root;
+        TreeNode* prev=NULL;
+        while(node){
+            if(val<node->val){
+                prev=node;
+                node=node->left;
+            }
+            else{
+                prev=node;
+                node=node->right;
+            }
+        }
+        if(val<prev->val){
+            TreeNode* newNode = new TreeNode(val);
+            prev->left=newNode;
+        }
+        else{
+            TreeNode* newNode = new TreeNode(val);
+            prev->right=newNode;
+        }
+        return root;
     }
 };
